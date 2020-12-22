5a845d23d919bce70e5e59cbc6d630c1a97f29f0
Abdullah Nahian <nahiansylhet@gmail.com>
2020-12-22 22:26:42 +0600

    Class 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,16 @@ $(document).ready(function(){
         'autoplay': true
     });
 });
+
+$(window).load(function () {
+
+    /* Sticky Header
+    ============================*/
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 1) {
+            $('.header-area').addClass("sticky");
+        } else {
+            $('.header-area').removeClass("sticky");
+        }
+    });
+});
