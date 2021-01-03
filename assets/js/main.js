d5bcc4985f1f329eef70645124c5b2f63beeab3e
Abdullah Nahian <nahiansylhet@gmail.com>
2021-01-03 20:57:28 +0600

    Class 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
+
+    /* Main Slider Active */
     $(".owl-carousel").owlCarousel({
         'items': 1,
         'loop': true,
         'autoplay': true
     });
+
+    /* CounterUp Active */
+    $('.counter').counterUp();
 });
 
 $(window).load(function () {
